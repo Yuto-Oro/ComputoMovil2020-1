18d96a4653504325908712572a821b9ba5e2dc17
Orlando Ortega <oro@Orlandos-MacBook-Pro.local>
2020-12-30 13:19:44 -0600

    Cambios en carpetas y ejercicio 1

---
+++ Program1/Program1/Recursivity.swift
@@ -0,0 +1,8 @@
+//
+//  Recursivity.swift
+//  Program1
+//
+//  Created by Orlando Ortega on 19/12/20.
+//
+
+import Foundation
---
+++ Program1/Program1/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Program1
+//
+//  Created by Orlando Ortega on 19/12/20.
+//
+
+import Foundation
+
+print("Hello, World!")
+
